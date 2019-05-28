04d7b3eede8fb592f3230fafd8a91b3014bbf86d
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-05-28 16:02:20 -0600

    Full game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,12 @@
 const words = ['fat', 'lazy', 'hungry'];
 const allowableLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let wins = 0;
-let guessesRemaining = 10;
+let guessesRemaining = 5;
 let currentWord = "";
 let underScore = [];
 let rightWord = [];
 let wrongWord = [];
+let gameStarted = false;
 
 
 //DOM manipulation
@@ -15,17 +16,36 @@ let docWinTracker = document.getElementsByClassName('wins-count');
 let docWrongGuess = document.getElementsByClassName('wrongGuess');
 let docRemainingGuess = document.getElementsByClassName('remainingGuess');
 
+//press any key to start
+restartGame();
+function restartGame(){
+    document.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === 13) {
+            gameStarted = true;
+            sendToStart();         
+        }
+      });
+};
+
+//if game started = true
+function sendToStart(){
+    if(gameStarted){
+        startGame();
+    }
+};
+//start game function
 function startGame(){
     assignCurrentWord(words);
+    docUnderScore[0].innerHTML = generateUnderScore().join(' ');
     setEventListeners();
-    checkIfUserLost();
-}
-startGame();
+};
 
+//function to assign a random word to variable currentWord
 function assignCurrentWord(array) {
     const index = Math.floor(Math.random() * ((array.length -1) - 0 + 1)) + 0;
     currentWord = array[index]
   };
+
 
 //create underscore length based on randomly selected word
 function generateUnderScore(){
@@ -35,43 +55,57 @@ function generateUnderScore(){
     return underScore;
 };
 
+//function for event listener and changes that come from key inputs 
 function setEventListeners(){
     //get user guess
     document.onkeyup = function(e){
-        //if users guess is right
-        if(currentWord.indexOf(e.key) > -1){
-            //push to rightWord Array
-            rightWord.push(e.key);
-            //replace underscore with right letter
-            underScore[currentWord.indexOf(e.key)] = e.key;
-            docUnderScore[0].innerHTML = underScore.join(' ');
-            //when all underscores have been filled, alert YOU WIN and increase wins by 1
-            if(underScore.join('') ==  currentWord){
-                wins++;
-                winAlert();
-                docWinTracker[0].innerHTML = wins;
-                startGame();
+        console.log(e.key);
+        if(allowableLetters.includes(e.key)){
+            if(currentWord.indexOf(e.key) > -1){
+                //push to rightWord array
+                rightWord.push(e.key);
+                //replace underscore with right letter
+                underScore[currentWord.indexOf(e.key)] = e.key;
+                docUnderScore[0].innerHTML = underScore.join(' ');
+                //when all underscore have been filled, answer is correct
+                if(underScore.join('') === currentWord){
+                    wins++;
+                    docWinTracker[0].innerHTML = wins;
+                    gameEnder();
+                }
+            }
+            else{
+                wrongWord.push(e.key);
+                docWrongGuess[0].innerHTML = wrongWord;
+                guessesRemaining = guessesRemaining -1;
+                docRemainingGuess[0].innerHTML = guessesRemaining;
+                checkIfUserLost();
             }
         }
-        //if users guess is wrong push to wrongWord array
         else{
-            wrongWord.push(e.key);
-            docWrongGuess[0].innerHTML = wrongWord;
-            guessesRemaining = guessesRemaining -1;
-            docRemainingGuess[0].innerHTML = guessesRemaining;
+            alert("You may only use letters A-Z")
         }
     }
 };
 
+//function to check if the player is out of guesses, if so they lost
 function checkIfUserLost(){
     if(guessesRemaining <= 0){
-      alert('You LOSE!')
+      alert('You LOST!');
+      gameEnder();
     }
 };
 
-function winAlert(){
-    alert("You WIN!");
+function gameEnder(){
+    gameStarted = false;
+    guessesRemaining = 5;
+    currentWord = "";
+    underScore = [];
+    rightWord = [];
+    wrongWord = [];
+    docRemainingGuess[0].innerHTML = guessesRemaining;
+    docWrongGuess[0].innerHTML = 0;
+    docUnderScore[0].innerHTML = '';
+
 };
 
-
-docUnderScore[0].innerHTML = generateUnderScore().join(' ');
